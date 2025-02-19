5a43534787a1fceb8fa461097436ff18e6a9d03e
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-02-19 01:05:08 +0000

    AS Key Word

--- quieries.sql
+++ quieries.sql
@@ -1,8 +1,10 @@
 USE sql_store;
 
-SELECT * FROM  customers WHERE customer_id =1 ORDER BY first_name;
+-- SELECT * FROM  customers WHERE customer_id =1 ORDER BY first_name;
 
 
-SELECT COLUMN_NAME
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'YourTableName';
+-- SELECT COLUMN_NAME
+-- FROM INFORMATION_SCHEMA.COLUMNS
+-- WHERE TABLE_NAME = 'YourTableName';
+
+SELECT name, unit_price, unit_price * 1.1 AS new_price FROM products;
